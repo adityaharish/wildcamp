7f80bf70c215575cba3344f03d7675c9ec733236
Aditya Harish <adityaharish@Adityas-MacBook-Pro.local>
2020-08-23 02:44:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds"),
+    port = process.env.PORT || 3000,
+    flash       = require("connect-flash"),
+    methodOverride = require("method-override");
+    
+//requring routes
+var commentRoutes    = require("./routes/comments"),
+    reviewRoutes     = require("./routes/reviews"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+    
+mongoose.connect("mongodb://localhost/wildcamp");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+//seedDB();
+app.locals.moment = require('moment');
+
+// PASSPORT CONFIGURATION-required to add authentication in any application
+
+app.use(require("express-session")({
+    secret: "Aditya Harish is crazy",
+    resave: false,
+    saveUninitialized: false
+}));
+//passport configuration-required for all applications
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//middleware for the currentUser to be used for all the routes
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
+
+app.listen(port, function () {
+  console.log("Server Has Started!");
+});
+
---
+++ routes/campgrounds.js
@@ -0,0 +1,182 @@
+var express = require("express");
+var router  = express.Router();
+var Campground = require("../models/campground");
+var middleware = require("../middleware")//index is the default in directory
+var Review = require("../models/review");
+
+
+
+//INDEX - show all campgrounds
+router.get("/", function(req, res){
+    var noMatch = null;
+    if(req.query.search) {
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        // Get all campgrounds from DB
+        Campground.find({name: regex}, function(err, allCampgrounds){
+           if(err){
+               console.log(err);
+           } else {
+              if(allCampgrounds.length < 1) {
+                  noMatch = "No campgrounds match that query, please try again.";
+              }
+              res.render("campgrounds/index",{campgrounds:allCampgrounds, noMatch: noMatch});
+           }
+        });
+    } else {
+        // Get all campgrounds from DB
+        Campground.find({}, function(err, allCampgrounds){
+           if(err){
+               console.log(err);
+           } else {
+              res.render("campgrounds/index",{campgrounds:allCampgrounds, noMatch: noMatch});
+           }
+        });
+    }
+});
+
+//CREATE - add new campground to DB
+router.post("/", middleware.isLoggedIn, function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var price = req.body.price
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, price: price, description: desc, author:author}
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            console.log(newlyCreated);
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+//NEW - show form to create new campground
+router.get("/new",middleware.isLoggedIn, function(req, res){
+   res.render("campgrounds/new"); 
+});
+
+// SHOW - shows more info about one campground
+router.get("/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments likes").populate({
+        path: "reviews",
+        options: {sort: {createdAt: -1}}
+    }).exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundCampground)
+            //render show template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+// Campground Like Route
+router.post("/:id/like", middleware.isLoggedIn, function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if (err) {
+            console.log(err);
+            return res.redirect("/campgrounds");
+        }
+
+        // check if req.user._id exists in foundCampground.likes
+        var foundUserLike = foundCampground.likes.some(function (like) {
+            return like.equals(req.user._id);
+        });
+
+        if (foundUserLike) {
+            // user already liked, removing like
+            foundCampground.likes.pull(req.user._id);
+        } else {
+            // adding the new user like
+            foundCampground.likes.push(req.user);
+        }
+
+        foundCampground.save(function (err) {
+            if (err) {
+                console.log(err);
+                return res.redirect("/campgrounds");
+            }
+            return res.redirect("/campgrounds/" + foundCampground._id);
+        });
+    });
+});
+
+
+
+//edit campground route
+router.get("/:id/edit" , middleware.checkCampgroundOwnership, function(req, res){
+    Campground.findById(req.params.id, function(err,foundCampground){
+        res.render("campgrounds/edit", {campground: foundCampground});
+    });   
+});
+
+//update campground route
+router.put("/:id",middleware.checkCampgroundOwnership, function(req, res){
+  delete req.body.campground.rating;
+    // find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+       if(err){
+           res.redirect("/campgrounds");
+       } else {
+           //redirect somewhere(show page)
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+    });
+});
+
+//destroy campground route
+// router.delete("/:id", middleware.checkCampgroundOwnership, function (req, res) {
+//     Campground.findById(req.params.id, function (err, campground) {
+//         if (err) {
+//             res.redirect("/campgrounds");
+//         } else {
+//             // deletes all comments associated with the campground
+//             Comment.remove({"_id": {$in: campground.comments}}, function (err) {
+//                 if (err) {
+//                     console.log(err);
+//                     return res.redirect("/campgrounds");
+//                 }
+//                 // deletes all reviews associated with the campground
+//                 Review.remove({"_id": {$in: campground.reviews}}, function (err) {
+//                     if (err) {
+//                         console.log(err);
+//                         return res.redirect("/campgrounds");
+//                     }
+//                     //  delete the campground
+//                     campground.remove();
+//                     req.flash("success", "Campground deleted successfully!");
+//                     res.redirect("/campgrounds");
+//                 });
+//             });
+//         }
+//     });
+// });
+router.delete("/:id",middleware.checkCampgroundOwnership, function(req, res){
+   Campground.findByIdAndRemove(req.params.id, function(err){
+      if(err){
+          res.redirect("/campgrounds");
+      } else {
+          res.redirect("/campgrounds");
+      }
+   });
+});
+
+
+//middleware
+//fuzzy search
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+
+module.exports = router;
